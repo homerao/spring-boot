f8a7a66ebfec5bd48390c1c5c85acbcd8215be61
Homero <homerao@gmail.com>
2020-06-30 23:23:29 -0300

    subindo controllers

--- web.mvc/src/main/java/com/onlinelearning/web/mvc/restcontroller/ApiAccessController.java
+++ web.mvc/src/main/java/com/onlinelearning/web/mvc/restcontroller/ApiAccessController.java
@@ -1,40 +1,13 @@
 package com.onlinelearning.web.mvc.restcontroller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.onlinelearning.web.mvc.model.ApiAccess;
-import com.onlinelearning.web.mvc.service.ApiAccessService;
-@RestController
-@RequestMapping(path = "/api/v1/api_access")
-@CrossOrigin(origins = "*")
+import com.onlinelearning.web.mvc.service.Service;
+
 public class ApiAccessController extends AbstractController<ApiAccess> {
 
-	private final ApiAccessService apiservice;
-	@Autowired
-	public ApiAccessController(ApiAccessService service) {
-
+	public ApiAccessController(Service<ApiAccess> service) {
 		super(service);
-		this.apiservice =  service;
-	}
-	@GetMapping(params = "{apiAccessId}")
-	public ResponseEntity<ApiAccess> getOne(@PathVariable(name = "apiAccessId") Long apiId){
-		ApiAccess api =  service.findOne(apiId);
-		return ResponseEntity.status(HttpStatus.OK).body(api);
-	}
-	
-	@GetMapping
-	public ResponseEntity<List<ApiAccess>> getOne(){
-		List<ApiAccess> api =  service.findAll();
-		return ResponseEntity.status(HttpStatus.OK).body(api);
+		
 	}
 
 }
--- web.mvc/src/main/java/com/onlinelearning/web/mvc/restcontroller/PaymentController.java
+++ web.mvc/src/main/java/com/onlinelearning/web/mvc/restcontroller/PaymentController.java
@@ -1,14 +1,8 @@
 package com.onlinelearning.web.mvc.restcontroller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.onlinelearning.web.mvc.model.Payment;
 import com.onlinelearning.web.mvc.service.Service;
-@RestController
-@RequestMapping(path = "/api/v1/payments")
-@CrossOrigin(origins = "*")
+
 public class PaymentController extends AbstractController<Payment> {
 
 	public PaymentController(Service<Payment> service) {
